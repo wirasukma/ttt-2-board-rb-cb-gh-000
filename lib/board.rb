49a9827aa3a5eccd903f3390a936b013a07ef9ad
Wira Sukma <wirasukma18@ummi.ac.id>
2018-07-20 10:54:43 +0000

    automatically backed up by learn

--- lib/board.rb
+++ lib/board.rb
@@ -1,8 +1,18 @@
 # Define the variable board below.
 
 board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+board[0]
+board[1]
+board[2]
+board[3]
+board[4]
+board[5]
+board[6]
+board[7]
+board[8]
+
 board[0] = " "
-board[1] = " "
+board[1] = "X"
 board[2] = " "
 board[3] = " "
 board[4] = " "
@@ -10,5 +20,3 @@ board[5] = " "
 board[6] = " "
 board[7] = " "
 board[8] = " "
-
-board[1] = "X"
