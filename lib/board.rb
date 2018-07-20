5eebcd38f8c6ea8a1bdf6b2c5e03d461ccffe0a0
Wira Sukma <wirasukma18@ummi.ac.id>
2018-07-20 11:02:40 +0000

    Done.

--- lib/board.rb
+++ lib/board.rb
@@ -1,1 +1,24 @@
 # Define the variable board below.
+
+board = [" "," "," "," "," "," "," "," "," "]
+#board[0]
+#board[1]
+#board[2]
+#board[3]
+#board[4]
+#board[5]
+#board[6]
+#board[7]
+#board[8]
+
+board[0] = " "
+board[1] = " "
+board[2] = " "
+board[3] = " "
+board[4] = " "
+board[5] = " "
+board[6] = " "
+board[7] = " "
+board[8] = " "
+
+board
